2ad6da5e2c76f3f10af34bd6dd97a0c702c0c7c7
yukang.chen <chenyukang@qq.com>
2014-03-31 16:35:12 +0800

    add kill_long_running_shark_client.sh script

---
+++ kill_long_running_shark_client.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: ${0##*/} <process name>"
+    exit 1
+fi
+
+proc=$1
+MAX_PROCESS_LIVE_TIME_IN_SECONDS=3600
+
+pids=`ps aux | grep $proc | grep -v grep | awk '{print $2}'`
+
+for p in $pids; do
+    read etime pid prog <<<$(ps -eo etime,pid,args | grep $p | grep $proc | grep -v grep)
+    etimeToSeconds=`echo $etime | awk -F $':' -f <(cat - <<-'EOF'
+  {
+    if (NF == 2) {
+      print $1*60 + $2
+    } else if (NF == 3) {
+      split($1, a, "-");
+      if (a[2] > 0) {
+        print ((a[1]*24+a[2])*60 + $2) * 60 + $3;
+      } else {
+        print ($1*60 + $2) * 60 + $3;
+      }
+    }
+  }
+EOF
+)`
+    if test -n "$etimeToSeconds";then
+	if [ $etimeToSeconds -gt $MAX_PROCESS_LIVE_TIME_IN_SECONDS ];then
+	    echo "PID:$pid PROGRAM:$prog running process more than ${MAX_PROCESS_LIVE_TIME_IN_SECONDS} seconds, killing process..." 
+            kill -9 $pid
+        fi
+    fi
+done
